b0f099e4b38da860caa4be924602ca8a0cf58a6a
ylahssini <ylahssini@gmail.com>
2021-07-24 20:01:57 +0100

    animate rotation of mars

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
+import { Stars, OrbitControls } from '@react-three/drei';
 import Mars from './components/mars';
 
 const App = () => {
@@ -7,6 +8,16 @@ const App = () => {
         <main>
             <Canvas>
                 <React.Suspense fallback={null}>
+                    <Stars radius={333} depth={1} />
+                    <pointLight color="#f8f8f0" position={[2,0,2]} intensity={0.85} />
+                    <OrbitControls
+                        enableZoom
+                        enableRotate
+                        enableRotate
+                        panSpeed={0.5}
+                        zoomSpeed={0.6}
+                        rotateSpeed={0.6}
+                    />
                     <Mars />
                 </React.Suspense>
             </Canvas>
--- src/components/mars.jsx
+++ src/components/mars.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { useLoader } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { useFrame, useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three';
-import { OrbitControls } from '@react-three/drei';
 import MarsNormalMap from '../assets/mars_10k_normal.jpg';
 import MarsColorlMap from '../assets/mars_10k_color.jpg';
 import MarsBumpMap from '../assets/mars_10k_topo.jpg';
@@ -9,21 +8,20 @@ import MarsBumpMap from '../assets/mars_10k_topo.jpg';
 const Mars = () => {
     const [normalMap, colorlMap, bumpMap,] = useLoader(TextureLoader, [MarsNormalMap, MarsColorlMap, MarsBumpMap]);
 
+    const marsRef = useRef();
+    useFrame(({ clock }) => {
+        const elapsed = clock.elapsedTime;
+        if (marsRef.current) {
+            marsRef.current.rotation.y = elapsed / 7;
+        }
+    });
+
     return (
         <>
-            <ambientLight intensity={0.5} />
-            <mesh>
+            <mesh ref={marsRef}>
                 <sphereGeometry args={[1, 50, 50]} />
-                <meshPhongMaterial color="crimson" wireframe />
+                <meshPhongMaterial specular={bumpMap} />
                 <meshStandardMaterial map={colorlMap} normalMap={normalMap} bumpMap={bumpMap} />
-                <OrbitControls
-                    enableZoom
-                    enableRotate
-                    enableRotate
-                    panSpeed={0.5}
-                    zoomSpeed={0.6}
-                    rotateSpeed={0.6}
-                />
             </mesh>
         </>
     )
